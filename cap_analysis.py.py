4dafb0ab631109f5615f206bf53066db5d0306fa
Mihir Alve <mihirsalve@gmail.com>
2019-04-05 22:13:04 -0400

    finished rough draft of analysis

--- cap_analysis.py.py
+++ cap_analysis.py.py
@@ -84,8 +84,9 @@ def plot_qb_salary_vs_wins(data):
     trendpoly = np.poly1d(trend)
     
     plt.plot(data['QB'], trendpoly(data['QB']), label="y={:.2f}x+{:.2f}".format(trend[0], trend[1]))
-    plt.legend() 
-    
+    plt.title("Wins vs. Combined Veteran Contract Quarterback Spending")
+#    plt.legend() 
+    print(trendpoly)
     plt.show()
     
 def remove_rookie_contracts(year, data, remove=True):
@@ -192,9 +193,10 @@ def plot_trenches(data):
     
     trend = np.polyfit(data['OL']+data['DL'], data['Wins'], 1)
     trendpoly = np.poly1d(trend)
+    print("OL & DL: " + str(trendpoly))
     
     plt.plot(data['OL']+data['DL'], trendpoly(data['OL']+data['DL']), label="y={:.2f}x+{:.2f}".format(trend[0], trend[1]))
-    plt.legend() 
+    plt.title("Wins vs. Combined OL/DL Spending")
     
     plt.show()
 
@@ -206,9 +208,10 @@ def plot_position(data, position):
     
     trend = np.polyfit(data[position], data['Wins'], 1)
     trendpoly = np.poly1d(trend)
+    print(position + ": " + str(trendpoly))
     
     plt.plot(data[position], trendpoly(data[position]), label="y={:.2f}x+{:.2f}".format(trend[0], trend[1]))
-    plt.legend() 
+    plt.title("Wins vs. " + position + " Spending")
     
     plt.show()
 
@@ -232,29 +235,54 @@ if __name__ == "__main__":
     year = "2018"
     data_2018 = get_data(year)
     
-    data_years = [data_2013, data_2014, data_2015, data_2016, data_2017, data_2018]
+#   Create overall combined table from data from 2013-2018
+    data_years = [data_2013, data_2014, data_2015, data_2016, data_2017]#, data_2018]
     data = pd.concat(data_years)
+
+#   Plot all qb spending vs. wins
 #    plot_qb_salary_vs_wins(data)
     
-    data_2013_2 = remove_rookie_contracts("2013", data_2013)
-    data_2014_2 = remove_rookie_contracts("2014", data_2014)
-    data_2015_2 = remove_rookie_contracts("2015", data_2015)
-    data_2016_2 = remove_rookie_contracts("2016", data_2016)
-    data_2017_2 = remove_rookie_contracts("2017", data_2017)
-    data_2018_2 = remove_rookie_contracts("2018", data_2018)
-    
-    data_years_2 = [data_2013_2, data_2014_2, data_2015_2, data_2016_2, data_2017_2, data_2018_2]
-    data_2 = pd.concat(data_years_2)
+#    Remove teams specified above with rookie contract QBS in the remove_rookie_contracts function 
+#    data_2013_2 = remove_rookie_contracts("2013", data_2013)
+#    data_2014_2 = remove_rookie_contracts("2014", data_2014)
+#    data_2015_2 = remove_rookie_contracts("2015", data_2015)
+#    data_2016_2 = remove_rookie_contracts("2016", data_2016)
+#    data_2017_2 = remove_rookie_contracts("2017", data_2017)
+#    data_2018_2 = remove_rookie_contracts("2018", data_2018)
+#    
+#    data_years_2 = [data_2013_2, data_2014_2, data_2015_2, data_2016_2, data_2017_2, data_2018_2]
+#    data_2 = pd.concat(data_years_2)
 #    plot_qb_salary_vs_wins(data_2)
     
-#    plot_offense_only(data)
-#    plot_defense_only(data)
+#    Reverse the remove_rookie_contrracs function and instead only keep teams with rookie contracts
+#    data_2013_2 = remove_rookie_contracts("2013", data_2013, False)
+#    data_2014_2 = remove_rookie_contracts("2014", data_2014, False)
+#    data_2015_2 = remove_rookie_contracts("2015", data_2015, False)
+#    data_2016_2 = remove_rookie_contracts("2016", data_2016, False)
+#    data_2017_2 = remove_rookie_contracts("2017", data_2017, False)
+#    data_2018_2 = remove_rookie_contracts("2018", data_2018, False)
+#    
+#    data_years_2 = [data_2013_2, data_2014_2, data_2015_2, data_2016_2, data_2017_2, data_2018_2]
+#    data_2 = pd.concat(data_years_2)
+#    
+##    Drop Jimmy G & the 49ers because he's a significant outlier
+##    data_2 = data_2[data_2['QB'] != 38197259]
+#    plot_qb_salary_vs_wins(data_2)
     
-#    plot_OL(data)
-#    plot_DL(data)
-#    plot_trenches(data)
+#    plot_position(data, "Offense")
+#    plot_position(data, "Defense")
     
-    plot_position
+    plot_position(data, "QB")
+    plot_position(data, "OL")
+    plot_position(data, "DL")
+    plot_trenches(data)
+    
+    plot_position(data, "RB")
+    plot_position(data, "WR")
+    plot_position(data, "TE")
+    plot_position(data, "LB")
+    plot_position(data, "S")
+    plot_position(data, 'CB')
 
 
 
